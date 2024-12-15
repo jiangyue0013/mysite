9b2fd490df9cf20bed66638af6ad176f576b97ac
jiangyue0013 <jiangyue0013@gmail.com>
2024-12-15 16:31:37 +0000

    Initial commit

---
+++ todo/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Task(models.Model):
+    # status choices: Pending, In Progress, and Completed.
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('in_progress', 'In Progress'),
+        ('completed', 'Completed'),
+    ]
+    # priority choices: Low, Medium, and High.
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    deadline = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='medium')
+
+    def __str__(self):
+        return self.title
---
+++ todo/views.py
@@ -0,0 +1,87 @@
+from django.shortcuts import render, redirect
+from django import forms
+from django.utils.timezone import now
+from django.urls import reverse
+from django.contrib import messages
+
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'deadline', 'status', 'priority']
+        widgets = {
+            'deadline': forms.DateTimeInput(attrs={
+                'type': 'datetime-local',
+                'class': 'form-control',
+                'placeholder': 'Select a date and time',
+            }),
+        }
+
+    # Custom verification methods
+    def clean_title(self):
+        # check the length of title
+        title = self.cleaned_data.get('title')
+        if len(title) > 200:
+            raise forms.ValidationError("The title must not exceed 200 characters!")
+        return title
+
+    def clean_deadline(self):
+        # check the deadline time
+        deadline = self.cleaned_data.get('deadline')
+        if deadline < now():
+            raise forms.ValidationError("The deadline can't be past time!")
+        return deadline
+
+# index view method: show all tasks
+def index(request):
+    latest_task_list = Task.objects.order_by("deadline")
+    context = {"latest_task_list": latest_task_list}
+    return render(request, "todo/index.html", context)
+
+# create task view method: create new task
+def create_task(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,"Task created successfully!")
+            return redirect(reverse('index'))
+        else:
+            messages.error(request, "Failed to create the task. Please check the input.")
+    else:
+        form = TaskForm()
+
+    return render(request, "todo/create_task.html", {'form': form})
+
+# deelete task by id
+def delete_task(request, task_id):
+    try:
+        task = Task.objects.get(id=task_id)
+        task.delete()
+        messages.success(request, "Task deleted successfully!")
+    except Task.DoesNotExist:
+        messages.success(request, "Task does not exist!")
+
+    return redirect(reverse('index'))
+
+# update task by id
+def update_task(request, task_id):
+    try:
+        task = Task.objects.get(id=task_id)
+    except Task.DoesNotExist:
+        messages.success(request, "Task does not exist!")
+        return redirect(reverse('index'))
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Task updated successfully!")
+            return redirect(reverse('index'))
+        else:
+            messages.error(request, "Failed to update the task. Please check the input.")
+    else:
+        form = TaskForm(instance=task)
+
+    return render(request, "todo/update_task.html", {'form': form, 'task': task})
