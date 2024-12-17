39a325a08dd8d89ab29e76a6108da2c815421817
jiangyue0013 <jiangyue0013@gmail.com>
2024-12-17 18:41:20 +0000

    Beautify page display

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("", include("todo.urls")),
     path("polls/", include("polls.urls")),
-    path("todo/", include("todo.urls")),
     path("admin/", admin.site.urls),
 ]
--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("poll", views.index, name="polls_index")
 ]
